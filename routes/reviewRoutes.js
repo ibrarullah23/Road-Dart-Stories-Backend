8e87c29950eef941999050a6db32f5541d24b5b4
ibrarullah23 <ibrarullah0023@gmail.com>
2025-05-06 23:36:38 +0500

    review validation

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { authMiddleware } from './../middlewares/authMiddleware.js';
 import { createReview, getAllReviews, deleteReview, bulkCreateReviews, updateReview } from '../controllers/reviewsController.js';
+import { validateCreateReview, validateUpdateReview } from '../validation/reviewValidation.js';
 
 const router = express.Router();
 
-router.post('/', authMiddleware, createReview);
+router.post('/', authMiddleware, validateCreateReview, createReview);
 router.post('/bulk', bulkCreateReviews);
 // router.get('/:business', authMiddleware, getAllReviews);
-router.get('/',authMiddleware, getAllReviews);
+router.get('/', authMiddleware, getAllReviews);
 router.delete('/:id', authMiddleware, deleteReview);
-router.patch('/:id', authMiddleware, updateReview);
+router.patch('/:id', authMiddleware, validateUpdateReview, updateReview);
 
 export default router;
---
+++ validation/reviewValidation.js
@@ -0,0 +1,45 @@
+import Joi from 'joi';
+import mongoose from 'mongoose';
+import validate from '../middlewares/validationMiddleware.js';
+
+// Helper to validate MongoDB ObjectId
+const objectId = Joi.string().custom((value, helpers) => {
+    if (!mongoose.Types.ObjectId.isValid(value)) {
+        return helpers.error('any.invalid');
+    }
+    return value;
+}, 'ObjectId Validation');
+
+
+
+const createReviewSchema = Joi.object({
+    // user: objectId.required(),
+    business: objectId.required(),
+    boardCondition: Joi.number().min(1).max(5).required(),
+    throwingLaneConditions: Joi.number().min(1).max(5).required(),
+    lightingConditions: Joi.number().min(1).max(5).required(),
+    spaceAllocated: Joi.number().min(1).max(5).required(),
+    gamingAmbience: Joi.number().min(1).max(5).required(),
+    img: Joi.string().uri().optional().messages({
+        'string.uri': 'Invalid URL format for img'
+    }),
+    text: Joi.string().required()
+});
+
+const updateReviewSchema = Joi.object({
+    // user: objectId.optional(),
+    // business: objectId.optional(),
+    boardCondition: Joi.number().min(1).max(5).optional(),
+    throwingLaneConditions: Joi.number().min(1).max(5).optional(),
+    lightingConditions: Joi.number().min(1).max(5).optional(),
+    spaceAllocated: Joi.number().min(1).max(5).optional(),
+    gamingAmbience: Joi.number().min(1).max(5).optional(),
+    img: Joi.string().uri().optional().messages({
+        'string.uri': 'Invalid URL format for img'
+    }),
+    text: Joi.string().optional()
+}).min(1); // Ensures at least one field is being updated
+
+export const validateUpdateReview = validate(updateReviewSchema);
+
+export const validateCreateReview = validate(createReviewSchema);
