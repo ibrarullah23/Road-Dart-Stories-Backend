9102ba07aff283de546bba3e5eb42f06b2dedc8b
ibrarullah23 <ibrarullah0023@gmail.com>
2025-05-22 23:16:15 +0500

    feat: refactor cookie options to use a constant for maxAge and add expiration date

--- constants/cookieOptions.js
+++ constants/cookieOptions.js
@@ -1,9 +1,13 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
+const weekInMs = 7 * 24 * 60 * 60 * 1000;
+
 export const cookieOptions = {
     httpOnly: true,
     secure: isProduction, // Use secure cookies only in production (HTTPS)
     sameSite: isProduction ? 'None' : 'Lax', // 'None' for cross-origin in production, 'Lax' for local dev
     path: '/',
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    maxAge: weekInMs, // 7 days in milliseconds
+    expires: new Date(Date.now() + weekInMs),
+    ...(isProduction ? { domain: ".roaddarts.com" } : {}),
 };
