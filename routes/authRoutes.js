ab74dece76d7e640e414e2d1ab05b13a2756023d
ibrarullah23 <ibrarullah0023@gmail.com>
2025-05-24 00:24:27 +0500

    feat: add forgot password and reset password routes to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,24 @@
 // routes/authRoutes.js
 import express from 'express';
-import { signup, logout, loginUser, googleAuth, getMe, updatePassword, verifyEmail, verifyEmailTemp, verifyCaptcha } from '../controllers/authController.js';
+import { signup, logout, loginUser, googleAuth, getMe, updatePassword, verifyEmail, verifyEmailTemp, verifyCaptcha, forgotPassword, resetPassword, resendVerificationEmail } from '../controllers/authController.js';
 import passport from "passport";
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
+
 router.post('/login', loginUser);
+router.post('/resend', resendVerificationEmail);
 router.post('/logout', authMiddleware, logout);
 router.post('/change-password', authMiddleware, updatePassword);
 router.get('/verify', verifyEmail);
 router.get('/tempverify', authMiddleware, verifyEmailTemp);
 router.get('/me', authMiddleware, getMe);
 router.get('/verify-captcha', verifyCaptcha);
+
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 // -------------------- Google OAuth --------------------
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] })); // Login, Redirect to Google for OAuth
