ac6d012f984e32e87cc7d5b13f9919087bcbea34
ibrarullah23 <ibrarullah0023@gmail.com>
2025-06-01 15:01:19 +0500

    feat: refactor email template structure and add support text for user assistance

--- config/mail.js
+++ config/mail.js
@@ -36,85 +36,85 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendMail = async (data) => {
 
 
-  // const header = `<!DOCTYPE html>
-  //   <html lang="en">
-  //   <head>
-  //     <meta charset="UTF-8">
-  //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  //     <title>Welcome to Road Dart</title>
+    // const header = `<!DOCTYPE html>
+    //   <html lang="en">
+    //   <head>
+    //     <meta charset="UTF-8">
+    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //     <title>Welcome to Road Dart</title>
 
-  //     <!-- Google Fonts -->
-  //     <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@600&family=Pacifico&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    //     <!-- Google Fonts -->
+    //     <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@600&family=Pacifico&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
 
-  //     <style>
-  //       body {
-  //         margin: 0;
-  //         padding: 0;
-  //         font-family: Arial, Helvetica, sans-serif;
-  //         background-color: #F7F8F9;
-  //         color: #000;
-  //       }
+    //     <style>
+    //       body {
+    //         margin: 0;
+    //         padding: 0;
+    //         font-family: Arial, Helvetica, sans-serif;
+    //         background-color: #F7F8F9;
+    //         color: #000;
+    //       }
 
-  //       .container {
-  //         max-width: 500px;
-  //         margin: 0 auto;
-  //         background-color: transparent;
-  //         padding: 10px;
-  //       }
+    //       .container {
+    //         max-width: 500px;
+    //         margin: 0 auto;
+    //         background-color: transparent;
+    //         padding: 10px;
+    //       }
 
-  //       .container img {
-  //         width: 100%;
-  //         height: auto;
-  //         max-width: 500px;
-  //         display: block;
-  //         margin: 0 auto;
-  //       }
+    //       .container img {
+    //         width: 100%;
+    //         height: auto;
+    //         max-width: 500px;
+    //         display: block;
+    //         margin: 0 auto;
+    //       }
 
-  //       h1 {
-  //         font-family: 'Pacifico', cursive;
-  //         font-size: 28px;
-  //         line-height: 1.4;
-  //         margin: 10px 0;
-  //       }
+    //       h1 {
+    //         font-family: 'Pacifico', cursive;
+    //         font-size: 28px;
+    //         line-height: 1.4;
+    //         margin: 10px 0;
+    //       }
 
-  //       p {
-  //         font-size: 14px;
-  //         font-family: 'Bitter', serif;
-  //         line-height: 1.4;
-  //         margin: 10px 0;
-  //         weight: 400;
-  //       }
+    //       p {
+    //         font-size: 14px;
+    //         font-family: 'Bitter', serif;
+    //         line-height: 1.4;
+    //         margin: 10px 0;
+    //         weight: 400;
+    //       }
 
-  //       .button {
-  //         display: inline-block;
-  //         background-color: #ebebeb;
-  //         text-decoration: none;
-  //         font-family: 'Ubuntu', sans-serif;
-  //         font-weight: 700;
-  //         font-size: 14px;
-  //         padding: 10px 20px;
-  //         border-radius: 4px;
-  //         margin-top: 10px;
-  //       }
-  //     </style>
-  //   </head>
-  //   <body>
+    //       .button {
+    //         display: inline-block;
+    //         background-color: #ebebeb;
+    //         text-decoration: none;
+    //         font-family: 'Ubuntu', sans-serif;
+    //         font-weight: 700;
+    //         font-size: 14px;
+    //         padding: 10px 20px;
+    //         border-radius: 4px;
+    //         margin-top: 10px;
+    //       }
+    //     </style>
+    //   </head>
+    //   <body>
 
-  //     <div class="container">
-  //     <img src="https://res.cloudinary.com/dfdlxyml9/image/upload/v1747941134/road-darts-logo_exdydk.png" alt="Road Dart" style="max-width: 200px; height: auto; display: block; margin: 0 auto;"  >
-  //     `
-  // <img src="https://res.cloudinary.com/dfdlxyml9/image/upload/v1747412127/roaddarts_wwhyzt.jpg" alt="Road Dart">
-  // const body = `    <h1>{{heading}}</h1>
+    //     <div class="container">
+    //     <img src="https://res.cloudinary.com/dfdlxyml9/image/upload/v1747941134/road-darts-logo_exdydk.png" alt="Road Dart" style="max-width: 200px; height: auto; display: block; margin: 0 auto;"  >
+    //     `
+    // <img src="https://res.cloudinary.com/dfdlxyml9/image/upload/v1747412127/roaddarts_wwhyzt.jpg" alt="Road Dart">
+    // const body = `    <h1>{{heading}}</h1>
 
-  //       <p>{{text}}</p>
+    //       <p>{{text}}</p>
 
-  //       <a href="{{buttonlink}}" class="button">{{buttontext}}</a>
-  //   `
-  // const footer = `</div> </body>
-  //   </html>`
+    //       <a href="{{buttonlink}}" class="button">{{buttontext}}</a>
+    //   `
+    // const footer = `</div> </body>
+    //   </html>`
 
 
-  const header = `<!DOCTYPE html>
+    const header = `<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -132,6 +132,14 @@ const sendMail = async (data) => {
             width: 100%;
             padding: 40px 0;
             background-color: #f7efff;
+        }
+
+        
+        .support-text{
+            font-size: 13px;
+            color: #353535;
+            line-height: 1.6;
+            margin: 10px 0;
         }
 
         .email-container {
@@ -202,7 +210,11 @@ const sendMail = async (data) => {
 
 
 
-  const footer = `</td>
+    const footer = `
+  <div class="support-text">
+                                Need help? <a href="mailto:support@roaddarts.com">Contact support</a>.
+                            </div>
+  </td>
                     </tr>
                 </table>
 
@@ -232,32 +244,32 @@ const sendMail = async (data) => {
 </html>`;
 
 
-  try {
-    // const mailOptions = {
-    //   from: process.env.EMAIL_USERNAME,
-    //   to: data.recipient,
-    //   subject: data.subject,
-    //   // text: "",
-    //   html: header + data.html + `</div>` + footer,
-    // };
-    // const transporter = getTransporter();
-    // await transporter.verify();
-    // console.log("Server is ready to take our messages");
-    // const info = await transporter.sendMail(mailOptions);
+    try {
+        // const mailOptions = {
+        //   from: process.env.EMAIL_USERNAME,
+        //   to: data.recipient,
+        //   subject: data.subject,
+        //   // text: "",
+        //   html: header + data.html + `</div>` + footer,
+        // };
+        // const transporter = getTransporter();
+        // await transporter.verify();
+        // console.log("Server is ready to take our messages");
+        // const info = await transporter.sendMail(mailOptions);
 
 
-    const response = await resend.emails.send({
-      from: 'Road Dart <support@roaddarts.com>',
-      to: data.recipient,
-      subject: data.subject,
-      html: header + data.html + footer,
-    });
+        const response = await resend.emails.send({
+            from: 'Road Dart <support@roaddarts.com>',
+            to: data.recipient,
+            subject: data.subject,
+            html: header + data.html + footer,
+        });
 
 
-    console.log(data.subject + " Email sent to ", data.recipient);
-  } catch (error) {
-    console.log("Error in mail.js", error.message);
-  }
+        console.log(data.subject + " Email sent to ", data.recipient);
+    } catch (error) {
+        console.log("Error in mail.js", error.message);
+    }
 };
 
 export default sendMail;
