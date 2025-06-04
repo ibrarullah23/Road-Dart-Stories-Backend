68393056982a3427dd720f6b8d65d87abae369e8
ibrarullah23 <ibrarullah0023@gmail.com>
2025-06-04 11:49:15 +0500

    feat: make lastname field optional in contactUs schema

--- models/ContactUs.js
+++ models/ContactUs.js
@@ -5,7 +5,7 @@ const { Schema, model } = mongoose;
 const contactUsSchema = new Schema(
   {
     firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
+    lastname: { type: String },
     email: { type: String, required: true },
     phone: { type: String },
     message: { type: String, required: true },
